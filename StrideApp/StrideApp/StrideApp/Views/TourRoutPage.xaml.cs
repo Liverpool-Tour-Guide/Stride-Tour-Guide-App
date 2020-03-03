b5dcca55c5a6b9b2d4c8ccfc840fece602ad24d1
mike_borman <beanborman@gmail.com>
2020-03-03 15:27:02 +0000

    Created a location object that should get the initial location then get location when moving - Not working

--- StrideApp/StrideApp/StrideApp/Views/TourRoutPage.xaml.cs
+++ StrideApp/StrideApp/StrideApp/Views/TourRoutPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Geolocator.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,14 @@ namespace StrideApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TourRoutPage : ContentPage
     {
+        public Position position { set; get; }
+        public location locator { set; get; }
         public TourRoutPage()
         {
             InitializeComponent();
+            locator = new location();
+            position = locator.position;
+            Labels.Text = position.Latitude.ToString();
         }
     }
 }
---
+++ StrideApp/StrideApp/StrideApp/location.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+
+
+namespace StrideApp
+{
+    public class location
+    {
+        public Position position { set; get; }
+        public location()
+        {
+            getInitLocation().Wait();
+            startListening();
+        }
+
+        private async Task getInitLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+
+            position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+        }
+
+        private async Task startListening()
+        {
+            await CrossGeolocator.Current.StartListeningAsync(TimeSpan.FromSeconds(5), 10, true);
+
+            CrossGeolocator.Current.PositionChanged += PositionChanged;
+        }
+
+        private void PositionChanged(object sender, PositionEventArgs e)
+        {
+            position = e.Position;
+        }
+    }
+}
