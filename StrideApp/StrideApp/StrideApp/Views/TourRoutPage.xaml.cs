cc907e311bc68ff4b42610c46875048e7afc18b0
mike_borman <beanborman@gmail.com>
2020-04-14 10:27:29 +0100

    Finished the static and non static listener for location and invoked the data on a main thread to constantly update on the tabbed page

--- StrideApp/StrideApp/StrideApp/Views/TourRoutPage.xaml.cs
+++ StrideApp/StrideApp/StrideApp/Views/TourRoutPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Geolocator;
 using Plugin.Geolocator.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,9 +19,26 @@ namespace StrideApp.Views
         public TourRoutPage()
         {
             InitializeComponent();
-            //locator = new location();
-            //position = locator.position;
-            //Labels.Text = position.Latitude.ToString();
+            locator = new location();
+            position = locator.position;
+
+			Long.Text = position.Longitude.ToString();
+            Lat.Text = position.Latitude.ToString();
+
+            // Starts a time on the main thread that calls the refresh function every 5 seconds
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                Device.BeginInvokeOnMainThread(() => refreshPosition());
+                return true;
+            });
         }
+
+        private void refreshPosition()
+        {
+            position = locator.position;
+            Long.Text = position.Longitude.ToString();
+            Lat.Text = position.Latitude.ToString();
+        }
+
     }
 }
--- StrideApp/StrideApp/StrideApp/location.cs
+++ StrideApp/StrideApp/StrideApp/location.cs
@@ -1,40 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Plugin.Geolocator;
 using Plugin.Geolocator.Abstractions;
 
 
+
 namespace StrideApp
 {
-    public class location
-    {
-        public Position position { set; get; }
-        public location()
-        {
-            getInitLocation().Wait();
-            //startListening();
-        }
+	public class location
+	{
+		public Position position { set; get; }
+		public location()
+		{
+			getInitLocation().Wait();
+			
+			Task.Run(async () => await StartListening());
+		}
 
-        private async Task getInitLocation()
-        {
-            var locator = CrossGeolocator.Current;
-            locator.DesiredAccuracy = 5;
+	
+		private async Task<Position> getInitLocation()
+		{
+			position = null;
+			try
+			{
+				var locator = CrossGeolocator.Current;
+				locator.DesiredAccuracy = 100;
 
-            position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
-        }
+				position = await locator.GetLastKnownLocationAsync();
 
-        private async Task startListening()
-        {
-            await CrossGeolocator.Current.StartListeningAsync(TimeSpan.FromSeconds(5), 10, true);
+				if (position != null)
+				{
+					//got a cahched position, so let's use it.
+					return position;
+				}
 
-            CrossGeolocator.Current.PositionChanged += PositionChanged;
-        }
+				if (!locator.IsGeolocationAvailable || !locator.IsGeolocationEnabled)
+				{
+					//not available or enabled
+					return null;
+				}
 
-        private void PositionChanged(object sender, PositionEventArgs e)
-        {
-            position = e.Position;
-        }
-    }
+				position = await locator.GetPositionAsync(TimeSpan.FromSeconds(20), null, true);
+
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Unable to get location: " + ex);
+			}
+
+			if (position == null)
+				return null;
+
+
+			return position;
+		}
+
+		async Task StartListening()
+		{
+			if (CrossGeolocator.Current.IsListening)
+				return;
+
+			await CrossGeolocator.Current.StartListeningAsync(TimeSpan.FromSeconds(5), 10, true);
+
+			CrossGeolocator.Current.PositionChanged += PositionChanged;
+			CrossGeolocator.Current.PositionError += PositionError;
+		}
+
+		private void PositionChanged(object sender, PositionEventArgs e)
+		{
+
+			
+			position = e.Position;
+			var output = "Full: Lat: " + position.Latitude + " Long: " + position.Longitude;
+			output += "\n" + $"Time: {position.Timestamp}";
+			output += "\n" + $"Heading: {position.Heading}";
+			output += "\n" + $"Speed: {position.Speed}";
+			output += "\n" + $"Accuracy: {position.Accuracy}";
+			output += "\n" + $"Altitude: {position.Altitude}";
+			output += "\n" + $"Altitude Accuracy: {position.AltitudeAccuracy}";
+			Debug.WriteLine(output);
+		}
+
+		private void PositionError(object sender, PositionErrorEventArgs e)
+		{
+			Debug.WriteLine(e.Error);
+			//Handle event here for errors
+		}
+
+	
+
+	}
 }
